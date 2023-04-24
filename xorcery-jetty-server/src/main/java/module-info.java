689db60483202a42b74d219a1e79a1df5a9b5118
kcg <kim.christian.gaarder@gmail.com>
2023-04-24 09:00:11 +0200

    Added requires health hk2 module to restore tests.

--- xorcery-jetty-server/src/main/java/module-info.java
+++ xorcery-jetty-server/src/main/java/module-info.java
@@ -9,6 +9,8 @@ open module xorcery.jetty.server {
     requires xorcery.keystores;
     requires xorcery.keystores.hk2;
     requires xorcery.health.api;
+    requires xorcery.health.registry;
+    requires xorcery.health.registry.hk2;
 
     requires jakarta.inject;
     requires jakarta.validation;
