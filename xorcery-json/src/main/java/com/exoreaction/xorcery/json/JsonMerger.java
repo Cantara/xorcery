5afd58a01e2231052680f55300b7f9d0f2b3d815
rickardoberg <rickardoberg@gmail.com>
2023-04-27 16:41:25 +0800

    Only use dot notation for names at root level

--- xorcery-json/src/main/java/com/exoreaction/xorcery/json/JsonMerger.java
+++ xorcery-json/src/main/java/com/exoreaction/xorcery/json/JsonMerger.java
@@ -11,7 +11,7 @@ import java.util.function.BiFunction;
 
 /**
  * Given a JSON tree, merge in another JSON tree into it and return the new tree.
- *
+ * <p>
  * Primitive values get overwritten by adding node, object values get current added with merged object,
  * and arrays get merged elements added to original array
  */
@@ -24,47 +24,50 @@ public class JsonMerger
     }
 
     public ObjectNode merge(ObjectNode current, ObjectNode adding) {
+        return merge0(current, adding, true);
+    }
+
+    public ObjectNode merge0(ObjectNode current, ObjectNode adding, boolean isRoot) {
         Iterator<Map.Entry<String, JsonNode>> fields = adding.fields();
-        fields: while (fields.hasNext()) {
+        fields:
+        while (fields.hasNext()) {
             JsonNode currentNode = current;
             Map.Entry<String, JsonNode> entry = fields.next();
 
             String key;
-            if (entry.getKey().startsWith("."))
-            {
+            if (entry.getKey().startsWith(".")) {
                 key = entry.getKey().substring(1);
-            } else
-            {
+            } else if (!isRoot) {
+                key = entry.getKey();
+            } else {
                 String[] keys = entry.getKey().split("\\.");
                 for (int i = 0; i < keys.length - 1; i++) {
                     JsonNode nextNode = currentNode.path(keys[i]);
-                    if (nextNode.isMissingNode())
-                    {
+                    if (nextNode.isMissingNode()) {
                         nextNode = current.objectNode();
                         ((ObjectNode) currentNode).set(keys[i], nextNode);
                     }
                     currentNode = nextNode;
                 }
-                key = keys[keys.length-1];
+                key = keys[keys.length - 1];
             }
 
             if (!currentNode.isObject())
                 continue;
 
-            ObjectNode currentObject = (ObjectNode)currentNode;
+            ObjectNode currentObject = (ObjectNode) currentNode;
             setValue(currentObject, key, entry.getValue());
         }
         return current;
     }
 
-    private void setValue(ObjectNode currentObject, String key, JsonNode value)
-    {
+    private void setValue(ObjectNode currentObject, String key, JsonNode value) {
         if (value instanceof ObjectNode addingObject) {
             JsonNode currentValue = currentObject.path(key);
             if (currentValue instanceof MissingNode) {
                 currentObject.set(key, value);
             } else if (currentValue instanceof ObjectNode currentValueObject) {
-                currentObject.set(key, apply(currentValueObject, addingObject));
+                currentObject.set(key, merge0(currentValueObject, addingObject, false));
             }
         } else if (value instanceof ArrayNode addingarray) {
             JsonNode currentValue = currentObject.path(key);
