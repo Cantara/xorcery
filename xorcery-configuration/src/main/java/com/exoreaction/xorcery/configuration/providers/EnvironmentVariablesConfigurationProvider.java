4899a4202f57ede193906c7f902deb2ae580cb32
rickardoberg <rickardoberg@gmail.com>
2023-06-22 18:54:36 +0800

    Environment variables and system properties are now interpreted as JSON/YAML when accessing from dynamic configuration providers

--- xorcery-configuration/src/main/java/com/exoreaction/xorcery/configuration/providers/EnvironmentVariablesConfigurationProvider.java
+++ xorcery-configuration/src/main/java/com/exoreaction/xorcery/configuration/providers/EnvironmentVariablesConfigurationProvider.java
@@ -1,19 +1,35 @@
 package com.exoreaction.xorcery.configuration.providers;
 
 import com.exoreaction.xorcery.configuration.spi.ConfigurationProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.MissingNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import java.io.UncheckedIOException;
 import java.util.Optional;
 
 public class EnvironmentVariablesConfigurationProvider
         implements ConfigurationProvider {
 
+    private final YAMLMapper yamlMapper;
+
+    public EnvironmentVariablesConfigurationProvider() {
+        yamlMapper = new YAMLMapper();
+    }
+
     @Override
     public JsonNode getJson(String name) {
         return Optional.ofNullable(System.getenv(name))
-                .<JsonNode>map(JsonNodeFactory.instance::textNode)
+                .map(value ->
+                {
+                    try {
+                        return yamlMapper.readTree(value);
+                    } catch (JsonProcessingException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
                 .orElseGet(MissingNode::getInstance);
     }
 
--- xorcery-configuration/src/main/java/com/exoreaction/xorcery/configuration/providers/SystemPropertiesConfigurationProvider.java
+++ xorcery-configuration/src/main/java/com/exoreaction/xorcery/configuration/providers/SystemPropertiesConfigurationProvider.java
@@ -1,19 +1,35 @@
 package com.exoreaction.xorcery.configuration.providers;
 
 import com.exoreaction.xorcery.configuration.spi.ConfigurationProvider;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.MissingNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
+import java.io.UncheckedIOException;
 import java.util.Optional;
 
 public class SystemPropertiesConfigurationProvider
         implements ConfigurationProvider {
 
+    private final YAMLMapper yamlMapper;
+
+    public SystemPropertiesConfigurationProvider() {
+        yamlMapper = new YAMLMapper();
+    }
+
     @Override
     public JsonNode getJson(String name) {
         return Optional.ofNullable(System.getProperty(name.replace('_', '.')))
-                .<JsonNode>map(JsonNodeFactory.instance::textNode)
+                .map(value ->
+                {
+                    try {
+                        return yamlMapper.readTree(value);
+                    } catch (JsonProcessingException e) {
+                        throw new UncheckedIOException(e);
+                    }
+                })
                 .orElseGet(MissingNode::getInstance);
     }
 
