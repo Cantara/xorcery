20a1ac299ffe55658b321a0b365ffabbd00dff82
rickardoberg <rickardoberg@gmail.com>
2024-02-01 16:59:52 +0800

    Support regexps for sampler rules

--- xorcery-opentelemetry-sdk/src/main/java/com/exoreaction/xorcery/opentelemetry/sdk/exporters/SampleRule.java
+++ xorcery-opentelemetry-sdk/src/main/java/com/exoreaction/xorcery/opentelemetry/sdk/exporters/SampleRule.java
@@ -11,36 +11,59 @@ import io.opentelemetry.sdk.trace.data.LinkData;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
 
-public record SampleRule(String name, SpanKind spanKind, List<Map.Entry<AttributeKey<?>, Object>> attributes) {
+public record SampleRule(Predicate<String> name, SpanKind spanKind,
+                         List<Map.Entry<AttributeKey<?>, Predicate<Object>>> attributes) {
 
     public static SampleRule create(ObjectNode json) {
         JsonElement jsonElement = () -> json;
-        List<Map.Entry<AttributeKey<?>, Object>> attributes = jsonElement
+        List<Map.Entry<AttributeKey<?>, Predicate<Object>>> attributes = jsonElement
                 .getObjectAs("attributes", SampleRule::toAttributes)
                 .map(attr -> attr.entrySet().stream().toList())
                 .orElse(null);
+        Predicate<String> namePredicate = null;
         String name = jsonElement.getString("name").orElse(null);
+        if (name != null) {
+            if (name.indexOf('*') != -1 || name.indexOf('+') != -1 || name.indexOf('?') != -1) {
+                Pattern regexp = Pattern.compile(name);
+                namePredicate = s -> regexp.matcher(s).matches();
+            } else {
+                namePredicate = s -> s.equals(name);
+            }
+        }
         SpanKind spanKind = jsonElement.getEnum("spanKind", SpanKind.class).orElse(null);
-        return new SampleRule(name, spanKind, attributes);
+        return new SampleRule(namePredicate, spanKind, attributes);
     }
 
-    private static Map<AttributeKey<?>, Object> toAttributes(ObjectNode objectNode) {
+    private static Map<AttributeKey<?>, Predicate<Object>> toAttributes(ObjectNode objectNode) {
 
-        Map<AttributeKey<?>, Object> attributes = new HashMap<>();
+        Map<AttributeKey<?>, Predicate<Object>> attributes = new HashMap<>();
         for (Map.Entry<String, JsonNode> property : objectNode.properties()) {
             switch (property.getValue().getNodeType()) {
                 case BOOLEAN -> {
-                    attributes.put(AttributeKey.booleanKey(property.getKey()), property.getValue().asBoolean());
+                    Boolean value = property.getValue().asBoolean();
+                    attributes.put(AttributeKey.booleanKey(property.getKey()), b -> b.equals(value));
                 }
                 case NUMBER -> {
-                    if (property.getValue().isFloatingPointNumber())
-                        attributes.put(AttributeKey.doubleKey(property.getKey()), property.getValue().asDouble());
-                    else
-                        attributes.put(AttributeKey.longKey(property.getKey()), property.getValue().asLong());
+                    if (property.getValue().isFloatingPointNumber()) {
+                        Double value = property.getValue().asDouble();
+                        attributes.put(AttributeKey.doubleKey(property.getKey()), d -> d.equals(value));
+                    } else {
+                        Long value = property.getValue().asLong();
+                        attributes.put(AttributeKey.longKey(property.getKey()), l -> l.equals(value));
+                    }
                 }
                 case STRING -> {
-                    attributes.put(AttributeKey.stringKey(property.getKey()), property.getValue().asDouble());
+                    String value = property.getValue().asText();
+                    // Try to guess if this is a regexp
+                    if (value.indexOf('*') != -1 || value.indexOf('+') != -1 || value.indexOf('?') != -1) {
+                        Pattern regexp = Pattern.compile(value);
+                        attributes.put(AttributeKey.stringKey(property.getKey()), s -> regexp.matcher(s.toString()).matches());
+                    } else {
+                        attributes.put(AttributeKey.stringKey(property.getKey()), s -> s.equals(value));
+                    }
                 }
             }
         }
@@ -50,7 +73,7 @@ public record SampleRule(String name, SpanKind spanKind, List<Map.Entry<Attribut
     public boolean matches(String name, SpanKind spanKind, Attributes attributes, List<LinkData> parentLinks) {
 
         if (this.name != null) {
-            if (!name.equals(this.name))
+            if (!this.name.test(name))
                 return false;
         }
         if (this.spanKind != null) {
@@ -59,11 +82,13 @@ public record SampleRule(String name, SpanKind spanKind, List<Map.Entry<Attribut
         }
         if (this.attributes != null) {
             for (int i = 0; i < this.attributes.size(); i++) {
-                Map.Entry<AttributeKey<?>, Object> entry = this.attributes.get(i);
+                Map.Entry<AttributeKey<?>, Predicate<Object>> entry = this.attributes.get(i);
                 Object value = attributes.get(entry.getKey());
                 if (value != null) {
-                    if (!value.equals(entry.getValue()))
+                    if (!entry.getValue().test(value))
                         return false;
+                } else {
+                    return false;
                 }
             }
         }
