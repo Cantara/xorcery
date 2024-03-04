62844352f209cdab652eb65d2d17de89d13fec30
rickardoberg <rickardoberg@gmail.com>
2024-03-04 17:02:55 +0800

    Turned into interface

--- xorcery-util/src/main/java/com/exoreaction/xorcery/lang/Classes.java
+++ xorcery-util/src/main/java/com/exoreaction/xorcery/lang/Classes.java
@@ -17,17 +17,107 @@ package com.exoreaction.xorcery.lang;
 
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.lang.reflect.TypeVariable;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 
-public final class Classes {
+public interface Classes {
 
-    public static Class<Object> getClass(Type type)
+    static Class<Object> getClass(Type type)
     {
         return (Class<Object>)(type instanceof ParameterizedType pt ? pt.getRawType() : type);
     }
 
-    public static <I,O> Function<I,O> cast(Class<O> clazz)
+    static <I,O> Function<I,O> cast(Class<O> clazz)
     {
         return clazz::cast;
+    }
+
+    /**
+     * From https://stackoverflow.com/questions/17297308/how-do-i-resolve-the-actual-type-for-a-generic-return-type-using-reflection
+     * <p>
+     * Resolves the actual generic type arguments for a base class, as viewed from a subclass or implementation.
+     *
+     * @param <T>        base type
+     * @param offspring  class or interface subclassing or extending the base type
+     * @param base       base class
+     * @param actualArgs the actual type arguments passed to the offspring class
+     * @return actual generic type arguments, must match the type parameters of the offspring class. If omitted, the
+     * type parameters will be used instead.
+     */
+    static <T> Type[] resolveActualTypeArgs(Class<? extends T> offspring, Class<T> base, Type... actualArgs) {
+
+        assert offspring != null;
+        assert base != null;
+        assert actualArgs.length == 0 || actualArgs.length == offspring.getTypeParameters().length;
+
+        //  If actual types are omitted, the type parameters will be used instead.
+        if (actualArgs.length == 0) {
+            actualArgs = offspring.getTypeParameters();
+        }
+        if (actualArgs.length == 0 && offspring.getTypeParameters().length > 0) {
+            actualArgs = ((ParameterizedType) offspring.getGenericSuperclass()).getActualTypeArguments();
+        }
+        // map type parameters into the actual types
+        Map<String, Type> typeVariables = new HashMap<String, Type>();
+        for (int i = 0; i < actualArgs.length; i++) {
+            TypeVariable<?> typeVariable = (TypeVariable<?>) offspring.getTypeParameters()[i];
+            typeVariables.put(typeVariable.getName(), actualArgs[i]);
+        }
+
+        // Find direct ancestors (superclass, interfaces)
+        List<Type> ancestors = new LinkedList<Type>();
+        if (offspring.getGenericSuperclass() != null) {
+            ancestors.add(offspring.getGenericSuperclass());
+        }
+        for (Type t : offspring.getGenericInterfaces()) {
+            ancestors.add(t);
+        }
+
+        // Recurse into ancestors (superclass, interfaces)
+        for (Type type : ancestors) {
+            if (type instanceof Class<?>) {
+                // ancestor is non-parameterized. Recurse only if it matches the base class.
+                Class<?> ancestorClass = (Class<?>) type;
+                if (base.isAssignableFrom(ancestorClass)) {
+                    Type[] result = resolveActualTypeArgs((Class<? extends T>) ancestorClass, base);
+                    if (result != null) {
+                        return result;
+                    }
+                }
+            }
+            if (type instanceof ParameterizedType) {
+                // ancestor is parameterized. Recurse only if the raw type matches the base class.
+                ParameterizedType parameterizedType = (ParameterizedType) type;
+                Type rawType = parameterizedType.getRawType();
+                if (rawType instanceof Class<?>) {
+                    Class<?> rawTypeClass = (Class<?>) rawType;
+                    if (base.isAssignableFrom(rawTypeClass)) {
+
+                        // loop through all type arguments and replace type variables with the actually known types
+                        List<Type> resolvedTypes = new LinkedList<Type>();
+                        for (Type t : parameterizedType.getActualTypeArguments()) {
+                            if (t instanceof TypeVariable<?>) {
+                                Type resolvedType = typeVariables.get(((TypeVariable<?>) t).getName());
+                                resolvedTypes.add(resolvedType != null ? resolvedType : t);
+                            } else {
+                                resolvedTypes.add(t);
+                            }
+                        }
+
+                        Type[] result = resolveActualTypeArgs((Class<? extends T>) rawTypeClass, base, resolvedTypes.toArray(new Type[]{}));
+                        if (result != null) {
+                            return result;
+                        }
+                    }
+                }
+            }
+        }
+
+        // we have a result if we reached the base class.
+        return offspring.equals(base) ? actualArgs : null;
     }
 }
