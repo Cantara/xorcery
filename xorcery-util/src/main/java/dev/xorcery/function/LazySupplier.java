f64c2bc35235ae9977d94a95866d542ff81105f0
rickardoberg <rickardoberg@gmail.com>
2024-11-04 13:49:27 +0800

    Added LazySupplier.java

---
+++ xorcery-util/src/main/java/dev/xorcery/function/LazySupplier.java
@@ -0,0 +1,33 @@
+package dev.xorcery.function;
+
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+
+/**
+ * Wraps a Supplier into a lazy accessor. Great for caching expensive optional calculations.
+ *
+ * @param <T>
+ */
+public class LazySupplier<T>
+        implements Supplier<T>
+{
+    private final Supplier<T> supplier;
+    private final AtomicReference<T> value = new AtomicReference<>();
+
+    private LazySupplier(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public static <T> Supplier<T> lazy(Supplier<T> supplier) {
+        return new LazySupplier<>(supplier);
+    }
+
+    public T get() {
+        return value.updateAndGet(v ->
+        {
+            if (v != null)
+                return v;
+            return supplier.get();
+        });
+    }
+}
