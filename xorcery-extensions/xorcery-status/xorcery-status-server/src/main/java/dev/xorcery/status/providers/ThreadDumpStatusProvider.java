02bb530ff0e8afcf21c553aa540d736559477325
rickardoberg <rickardoberg@gmail.com>
2025-11-05 17:52:45 +0800

    Make max frames configurable, default to 30

--- xorcery-extensions/xorcery-status/xorcery-status-server/src/main/java/dev/xorcery/status/providers/ThreadDumpStatusProvider.java
+++ xorcery-extensions/xorcery-status/xorcery-status-server/src/main/java/dev/xorcery/status/providers/ThreadDumpStatusProvider.java
@@ -15,10 +15,14 @@
  */
 package dev.xorcery.status.providers;
 
+import dev.xorcery.configuration.Configuration;
 import dev.xorcery.jsonapi.Attributes;
 import dev.xorcery.status.spi.StatusProvider;
+import jakarta.inject.Inject;
 import org.jvnet.hk2.annotations.Service;
 
+import java.lang.management.LockInfo;
+import java.lang.management.MonitorInfo;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.function.Predicate;
@@ -26,6 +30,13 @@ import java.util.function.Predicate;
 @Service(name="status.threads")
 public class ThreadDumpStatusProvider
         implements StatusProvider {
+
+    private final Integer maxFrames;
+
+    @Inject
+    public ThreadDumpStatusProvider(Configuration configuration) {
+        maxFrames = configuration.getInteger("status.threads.maxFrames").orElse(30);
+    }
 
     @Override
     public String getId() {
@@ -60,5 +71,74 @@ public class ThreadDumpStatusProvider
             attrs.attribute(thread.getName(), threadDump.toString());
         }
 */
+    }
+
+    public String toString(ThreadInfo info ) {
+        StringBuilder sb = new StringBuilder("\"" + info.getThreadName() + "\"" +
+                (info.isDaemon() ? " daemon" : "") +
+                " prio=" + info.getPriority() +
+                " Id=" + info.getThreadId() + " " +
+                info.getThreadState());
+        if (info.getLockName() != null) {
+            sb.append(" on " + info.getLockName());
+        }
+        if (info.getLockOwnerName() != null) {
+            sb.append(" owned by \"" + info.getLockOwnerName() +
+                    "\" Id=" + info.getLockOwnerId());
+        }
+        if (info.isSuspended()) {
+            sb.append(" (suspended)");
+        }
+        if (info.isInNative()) {
+            sb.append(" (in native)");
+        }
+        sb.append('\n');
+        int i = 0;
+        for (; i < info.getStackTrace().length && i < maxFrames; i++) {
+            StackTraceElement ste = info.getStackTrace()[i];
+            sb.append("\tat " + ste.toString());
+            sb.append('\n');
+            if (i == 0 && info.getLockInfo() != null) {
+                Thread.State ts = info.getThreadState();
+                switch (ts) {
+                    case BLOCKED:
+                        sb.append("\t-  blocked on " + info.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    case WAITING:
+                        sb.append("\t-  waiting on " + info.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    case TIMED_WAITING:
+                        sb.append("\t-  waiting on " + info.getLockInfo());
+                        sb.append('\n');
+                        break;
+                    default:
+                }
+            }
+
+            for (MonitorInfo mi : info.getLockedMonitors()) {
+                if (mi.getLockedStackDepth() == i) {
+                    sb.append("\t-  locked " + mi);
+                    sb.append('\n');
+                }
+            }
+        }
+        if (i < info.getStackTrace().length) {
+            sb.append("\t...");
+            sb.append('\n');
+        }
+
+        LockInfo[] locks = info.getLockedSynchronizers();
+        if (locks.length > 0) {
+            sb.append("\n\tNumber of locked synchronizers = " + locks.length);
+            sb.append('\n');
+            for (LockInfo li : locks) {
+                sb.append("\t- " + li);
+                sb.append('\n');
+            }
+        }
+        sb.append('\n');
+        return sb.toString();
     }
 }
